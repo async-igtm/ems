84df61c705d59c6f49ba142d23c34432066b00a0
Mr. Async <126945779+async-igtm@users.noreply.github.com>
2024-10-20 14:55:33 +0530

    Update @index_main.js
    sdf

--- _@view/_js/@index_main.js
+++ _@view/_js/@index_main.js
@@ -1,13 +1,35 @@
+// var img = [
+//     '@18771.gif',
+//     '@19873.gif',
+//     '@89357.gif'
+// ];
+
+// var i = Math.floor(Math.random() * img.length);
+// var x = img[i];
+
+
+// var link = "_ignator/_@view/_Utilities/" + x;
+// document.getElementById('img_t').setAttribute('src', link);
+
+
+// var img = [
+//     '@18771.gif',
+//     '@19873.gif',
+//     '@89357.gif'
+// ];
+
 var img = [
-    '@18771.gif',
-    '@19873.gif',
-    '@89357.gif'
+    'https://cdn.jsdelivr.net/gh/async-igtm/ems@1529f2cf8fcad11061ede14b130abe7ed8c6f7ee/_@40view/_Utilities/@18771.gif',
+    'https://cdn.jsdelivr.net/gh/async-igtm/ems@1529f2cf8fcad11061ede14b130abe7ed8c6f7ee/_@view/_Utilities/@19873.gif',
+    'https://cdn.jsdelivr.net/gh/async-igtm/ems@1529f2cf8fcad11061ede14b130abe7ed8c6f7ee/_@view/_Utilities/@89357.gif'
 ];
+
 
 var i = Math.floor(Math.random() * img.length);
 var x = img[i];
 
-
-var link = "_ignator/_@view/_Utilities/" + x;
+var link = x;
+// var link = "_ignator/_@view/_Utilities/" + x;
 document.getElementById('img_t').setAttribute('src', link);
+
 
